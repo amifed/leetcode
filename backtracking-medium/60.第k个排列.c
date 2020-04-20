ac9dad9159a1680422d03e5ff3d6db4c0a78e271
dingjinyang <djy.dingjinyang@gmail.com>
2020-04-20 22:04:17 +0800

    60

--- backtracking-medium/60.第k个排列.c
+++ backtracking-medium/60.第k个排列.c
@@ -51,11 +51,28 @@
 
 // @lc code=start
 
-
-char * getPermutation(int n, int k){
-
+int factorial(int n) {
+  int num = 1;
+  while (n > 0)
+    num *= n--;
+  return num;
+}
+void deleteItem(int *nums, int numsSize, int in) {
+  while (in < numsSize - 1)
+    nums[in++] = nums[in + 1];
+}
+char *getPermutation(int n, int k) {
+  int i, j = 0, nums[n], factor;
+  char *res = (char *)calloc(10, sizeof(char));
+  for (i = 0; i < n; i++)  //初始化一个 [1,2,3,……,n] 数组
+    nums[i] = i + 1;
+  for (i = 0, k--; i < n; i++) {  //k要先减去1
+    factor = factorial(n - i - 1);
+    res[j++] = nums[k / factor] + '0';
+    deleteItem(nums, n - i, k / factor);  //取出一个元素
+    k %= factor;
+  }
+  return res;
 }
 
-
 // @lc code=end
-
