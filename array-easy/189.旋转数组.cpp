1ea524413599117684d2a2ffb3067f7d9b2558e4
dingjinyang <djy.dingjinyang@gmail.com>
2020-05-07 20:34:17 +0800

    189

---
+++ array-easy/189.旋转数组.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=189 lang=cpp
+ *
+ * [189] 旋转数组
+ */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
+class Solution {
+  public:
+  void rotate(vector<int>& nums, int k) {
+    // k %= nums.size();
+    // reverse(nums.begin(), nums.end() - k);
+    // reverse(nums.end() - k, nums.end());
+    // reverse(nums.begin(), nums.end());
+    std::rotate(nums.begin(), nums.ends() - k % nums.size(), nums.end());
+  }
+};
+// @lc code=end
