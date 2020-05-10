af121f0fa29ff1d82ab9d9d3288c95cd57d5c0ca
dingjinyang <djy.dingjinyang@gmail.com>
2020-05-10 21:49:47 +0800

    236 daily problem

--- tree-medium/236.二叉树的最近公共祖先.cpp
+++ tree-medium/236.二叉树的最近公共祖先.cpp
@@ -20,6 +20,10 @@ using namespace std;  // namespace std;
 class Solution {
   public:
   TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if (root == NULL || root == p || root == q) return root;
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+    return left == NULL ? right : right == NULL ? left : root;
   }
 };
 // @lc code=end
