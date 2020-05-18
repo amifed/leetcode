32aec7d8a69cbd0530b2ed888d45300500531903
dingjinyang <djy.dingjinyang@gmail.com>
2020-05-18 23:03:57 +0800

    1037

---
+++ math-easy/1037.有效的回旋镖.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=1037 lang=cpp
+ *
+ * [1037] 有效的回旋镖
+ */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+  bool isBoomerang(vector<vector<int>> &points) {
+    return (points[1][1] - points[0][1]) * (points[2][0] - points[0][0]) !=
+           (points[2][1] - points[0][1]) * (points[1][0] - points[0][0]);
+  }
+};
+// @lc code=end
