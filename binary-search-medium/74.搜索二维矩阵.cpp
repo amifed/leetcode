be427df4433dfb55fc15fb2a2a9261b82447fa37
dingjinyang <djy.dingjinyang@gmail.com>
2020-05-19 11:41:21 +0800

    74

---
+++ binary-search-medium/74.搜索二维矩阵.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=74 lang=cpp
+ *
+ * [74] 搜索二维矩阵
+ */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+  bool searchMatrix(vector<vector<int>> &matrix, int target) {
+    int row = matrix.size(), col = matrix.size() > 0 ? matrix[0].size() : 0;
+    if (row == 0 || col == 0)
+      return false;
+    int left = 0, right = row * col - 1;
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      int element = matrix[mid / col][mid % col];
+      if (element > target)
+        right = mid - 1;
+      else if (element < target)
+        left = mid + 1;
+      else
+        return true;
+    }
+    return false;
+  }
+};
+// @lc code=end
