3f30aa4cc222541420d56ef8bb082a70f8bc0eab
jinyang <djy.dingjinyang@gmail.com>
2020-05-21 21:47:57 +0800

    ListNode

--- ListNode.h
+++ ListNode.h
@@ -1,42 +1,35 @@
-struct ListNode
-{
-    int val;
-    struct ListNode *next;
+struct ListNode {
+  int val;
+  struct ListNode *next;
 };
 
 /**
  * 尾插法 创建单链表 （无头结点）
  * @params int n 结点个数
  **/
-struct ListNode *tailInsertCreateList(int n)
-{
-    struct ListNode *r = malloc(sizeof(struct ListNode));
-    struct ListNode *L = r;
-    for (int i = 0; i < n; ++i)
-    {
-        scanf("%d", &r->val);
-        if (i != n - 1)
-        {
-            struct ListNode *p = (struct ListNode *)malloc(sizeof(struct ListNode));
-            r->next = p;
-            r = p;
-        }
-        else
-            r->next = NULL;
-    }
-    return L;
+struct ListNode *tailInsertCreateList(int n) {
+  struct ListNode *r = malloc(sizeof(struct ListNode));
+  struct ListNode *L = r;
+  for (int i = 0; i < n; ++i) {
+    scanf("%d", &r->val);
+    if (i != n - 1) {
+      struct ListNode *p = (struct ListNode *)malloc(sizeof(struct ListNode));
+      r->next = p;
+      r = p;
+    } else
+      r->next = NULL;
+  }
+  return L;
 }
 
 /**
- * 输出单链表 
+ * 输出单链表
  **/
-void outputLinkedList(struct ListNode *L)
-{
-    struct ListNode *p = L;
-    while (p != NULL)
-    {
-        printf("%d -> ", p->val);
-        p = p->next;
-    }
-    printf("NULL\n");
+void outputLinkedList(struct ListNode *L) {
+  struct ListNode *p = L;
+  while (p != NULL) {
+    printf("%d -> ", p->val);
+    p = p->next;
+  }
+  printf("NULL\n");
 }
