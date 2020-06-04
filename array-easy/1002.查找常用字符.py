5e41ed3d63373dd92a8a4878517baea671d1ba88
jinyang <djy.dingjinyang@gmail.com>
2020-06-04 19:34:44 +0800

    1002

---
+++ array-easy/1002.查找常用字符.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=1002 lang=python3
+#
+# [1002] 查找常用字符
+#
+from typing import List
+import collections
+
+
+# @lc code=start
+class Solution:
+    # def commonChars(self, A: List[str]) -> List[str]:
+    #     ret = list()
+    #     for c in set(A[0]):
+    #         for i in c * min([word.count(c) for word in A]):
+    #             ret.append(i)
+    #     return ret
+    def commonChars(self, A: List[str]) -> List[str]:
+        ret = collections.Counter(A[0])
+        for i in range(1, len(A)):
+            ret &= collections.Counter(A[i])
+        return ret.elements()
+# @lc code=end
