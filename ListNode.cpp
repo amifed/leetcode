e3dcecde6bb9500f8afdf2076c672205e0c7de9c
jinyang <djy.dingjinyang@gmail.com>
2020-06-17 17:00:57 +0800

    ListNode

--- ListNode.cpp
+++ ListNode.cpp
@@ -1,33 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode(int x) : val(x), next(NULL) {}
+    int val;
+    ListNode *next;
+    ListNode(int x = 0) : val(x), next(NULL) {}
 };
 /**
  * 尾插法 创建单链表 （无头结点）
  * @params vector<int> 结点个数
  **/
 ListNode *tailInsertCreateList(vector<int> nodes) {
-  ListNode *L = new ListNode(0);
-  ListNode *r = L;
-  for (auto &&i : nodes) {
-    ListNode *p = new ListNode(i);
-    r->next = p;
-    r = p;
-  }
-  r->next = NULL;
-  return L->next;
+    ListNode *L = new ListNode;
+    ListNode *r = L;
+    for (auto &&i : nodes) {
+        ListNode *p = new ListNode(i);
+        r->next = p;
+        r = p;
+    }
+    r->next = NULL;
+    return L->next;
 }
+ListNode *headInsertCreateList(vector<int> nodes){}
 /**
  * 输出单链表
  **/
 void output(ListNode *L) {
-  ListNode *p = L;
-  while (p != NULL) {
-    printf("%d -> ", p->val);
-    p = p->next;
-  }
-  printf("NULL\n");
+    ListNode *p = L;
+    while (p != NULL) {
+        printf("%d -> ", p->val);
+        p = p->next;
+    }
+    printf("NULL\n");
 }
