7aefd69b6f3dcc480380216859ceaf7fc42b959b
dingjinyang <djy.dingjinyang@gmail.com>
2020-07-04 18:07:15 +0800

    32 with stack

--- dynamic-programming-hard/32.最长有效括号.py
+++ dynamic-programming-hard/32.最长有效括号.py
@@ -6,16 +6,33 @@
 
 # @lc code=start
 class Solution:
+    # def longestValidParentheses(self, s: str) -> int:
+    #    """dp"""
+    #     dp = [0] * len(s)
+    #     ret = 0
+    #     for i in range(1, len(s)):
+    #         if s[i] == ')':
+    #             if s[i-1] == '(':
+    #                 dp[i] = 2 + (dp[i-2] if i - 2 > -1 else 0)
+    #             elif i - dp[i-1] > 0 and s[i - dp[i-1] - 1] == '(':
+    #                 dp[i] = 2 + dp[i-1] + (dp[i - dp[i-1] - 2] if i -
+    #                                        dp[i-1] - 2 > -1 else 0)
+    #             ret = max(ret, dp[i])
+    #     return ret
+
     def longestValidParentheses(self, s: str) -> int:
-        dp = [0] * len(s)
+        """stack"""
         ret = 0
-        for i in range(1, len(s)):
-            if s[i] == ')':
-                if s[i-1] == '(':
-                    dp[i] = 2 + (dp[i-2] if i - 2 > -1 else 0)
-                elif i - dp[i-1] > 0 and s[i - dp[i-1] - 1] == '(':
-                    dp[i] = 2 + dp[i-1] + (dp[i - dp[i-1] - 2] if i -
-                                           dp[i-1] - 2 > -1 else 0)
-                ret = max(ret, dp[i])
+        stack = [-1]
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if stack:
+                    ret = max(ret, i - stack[-1])
+                else:
+                    stack.append(i)
         return ret
+
 # @lc code=end
