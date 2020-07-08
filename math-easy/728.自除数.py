e320f1ff852440fb7c00b8d3f1e68025f8873ac3
dingjinyang <djy.dingjinyang@gmail.com>
2020-07-08 18:43:14 +0800

    728

---
+++ math-easy/728.自除数.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=728 lang=python3
+#
+# [728] 自除数
+#
+from typing import List`
+
+
+# @lc code=start
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        ret = list()
+        for i in range(left, right+1):
+            s = str(i)
+            for c in s:
+                if c == '0' or i % int(c) != 0:
+                    break
+            else:
+                ret.append(i)
+        return ret
+# @lc code=end
