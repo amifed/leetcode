d09b69df9b25cd7bc9de67a54414d0ee88058f09
dingjinyang <djy.dingjinyang@gmail.com>
2020-07-16 13:57:39 +0800

    794

--- depth-first-search-medium/794.有效的井字游戏.cpp
+++ depth-first-search-medium/794.有效的井字游戏.cpp
@@ -8,30 +8,33 @@ using namespace std;
 // @lc code=start
 class Solution {
 private:
-    unordered_set<pair<int, int>> us;
-    bool isValid(vector<string> &board, char chess) {
+    bool isWin(vector<string> &board, char play) {
         for (int i = 0; i < 3; i++) {
-            if (board[i][0] == chess && board[i][1] == chess && board[i][2] == chess)
+            if (board[i][0] == play && board[i][1] == play && board[i][2] == play)
                 return true;
-            if (board[0][i] == chess && board[1][i] == chess && board[2][i] == chess)
+            if (board[0][i] == play && board[1][i] == play && board[2][i] == play)
                 return true;
         }
-        if (board[0][0] == chess && board[1][1] == chess && board[2][2] == chess)
+        if (board[0][0] == play && board[1][1] == play && board[2][2] == play)
             return true;
-        if (board[0][2] == chess && board[1][1] == chess && board[2][0] == chess)
+        if (board[0][2] == play && board[1][1] == play && board[2][0] == play)
             return true;
         return false;
-    }
-    bool recursive(vector<string> &board, char chess) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (us.count({i, j}) == 0)
-            }
-        }
     }
 
 public:
     bool validTicTacToe(vector<string> &board) {
+        int x = 0, o = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                x += board[i][j] == 'X';
+                o += board[i][j] == 'O';
+            }
+        }
+        if (x != o && x != o + 1) return false;
+        if (isWin(board, 'X') && x != o + 1) return false;
+        if (isWin(board, 'O') && x != o) return false;
+        return true;
     }
 };
 // @lc code=end
