f414155400a3e19eb240dbc2a8e3c6e4c267706e
dingjinyang <djy.dingjinyang@gmail.com>
2020-07-24 10:18:49 +0800

    724

---
+++ array-easy/724.寻找数组的中心索引.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=724 lang=cpp
+ *
+ * [724] 寻找数组的中心索引
+ */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    int pivotIndex(vector<int> &nums) {
+        int prefix = 0;
+        int sum = 0;
+        for (auto num : nums)
+            sum += num;
+        for (int i = 0; i < nums.size(); i++) {
+            if (prefix == sum - prefix - nums[i])
+                return i;
+            prefix += nums[i];
+        }
+        return -1;
+    }
+};
+// @lc code=end
