2aac5c0850ec9330635597c4350aa872e4e989a8
dingjinyang <djy.dingjinyang@gmail.com>
2020-07-31 00:06:10 +0800

    74

--- binary-search-medium/74.搜索二维矩阵.cpp
+++ binary-search-medium/74.搜索二维矩阵.cpp
@@ -8,22 +8,21 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
-  bool searchMatrix(vector<vector<int>> &matrix, int target) {
-    int row = matrix.size(), col = matrix.size() > 0 ? matrix[0].size() : 0;
-    if (row == 0 || col == 0)
-      return false;
-    int left = 0, right = row * col - 1;
-    while (left <= right) {
-      int mid = (left + right) / 2;
-      int element = matrix[mid / col][mid % col];
-      if (element > target)
-        right = mid - 1;
-      else if (element < target)
-        left = mid + 1;
-      else
-        return true;
-    }
-    return false;
-  }
+    bool searchMatrix(vector<vector<int>> &matrix, int target) {
+        int row = matrix.size(), col = matrix.size() == 0 ? 0 : matrix[0].size();
+        if (row == 0 || col == 0) return false;
+        int left = 0, right = row * col - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            int x = mid / col, y = mid % col;
+            if (matrix[x][y] < target)
+                left = mid + 1;
+            else if (matrix[x][y] > target)
+                right = mid - 1;
+            else
+                return true;
+        }
+        return false;
+    };
 };
 // @lc code=end
