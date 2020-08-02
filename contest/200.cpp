88f68fbff73c06bfee8f882fc1d53ed5f048a832
dingjinyang <djy.dingjinyang@gmail.com>
2020-08-02 16:44:09 +0800

    leetcode weekly contest 200

--- contest/200.cpp
+++ contest/200.cpp
@@ -55,3 +55,32 @@ public:
         return ret;
     }
 }; */
+
+#define mod 1000000007
+class Solution {
+public:
+    int maxSum(vector<int> &nums1, vector<int> &nums2) {
+        long long sum1 = 0, sum2 = 0;
+        long long ret = 0;
+        int i = 0, j = 0;
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] == nums2[j]) {
+                ret += max(sum1, sum2) + nums1[i];
+                sum1 = 0, sum2 = 0;
+                i++, j++;
+            } else if (nums1[i] < nums2[j]) {
+                sum1 += nums1[i++];
+            } else {
+                sum2 += nums2[j++];
+            }
+        }
+        while (i < nums1.size()) {
+            sum1 += nums1[i++];
+        }
+        while (j < nums2.size()) {
+            sum2 += nums2[j++];
+        }
+        ret += max(sum1, sum2);
+        return ret & mod;
+    }
+};
