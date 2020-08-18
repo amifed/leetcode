bb9fed95e7d6b29080b28e365c8ae90e4629fc4f
dingjinyang <djy.dingjinyang@gmail.com>
2020-08-18 14:00:18 +0800

    update contestTemplate

--- contestTemplate.cpp
+++ contestTemplate.cpp
@@ -1,13 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define all(v) (v).begin(), (v).end()
 using LL = long long;
 using PII = pair<int, int>;
 using PIL = pair<int, LL>;
 using PLL = pair<LL, LL>;
 const int mod = 1e9 + 7;
+const int inf = 1e9 + 7;
 
 class Solution {
 public:
+
 };
 
 int main() {
