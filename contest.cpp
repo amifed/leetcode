aeb868e5db410e896208dd7f607f2ef6db0a61b8
dingjinyang <djy.dingjinyang@gmail.com>
2020-08-18 09:45:00 +0800

    109 contest template

---
+++ contest.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+using LL = long long;
+using PII = pair<int, int>;
+using PIL = pair<int, LL>;
+using PLL = pair<LL, LL>;
+const int mod = 1e9 + 7;
+
+class Solution {
+public:
+};
+
+int main() {
+    Solution solution;
+
+    return 0;
+}
---
+++ depth-first-search-medium/109.有序链表转换二叉搜索树.cpp
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=109 lang=cpp
+ *
+ * [109] 有序链表转换二叉搜索树
+ */
+#include <bits/stdc++.h>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *build(ListNode *&head, int left, int right) {
+        if (left > right) {
+            return nullptr;
+        }
+        int mid = (left + right + 1) >> 1;
+        TreeNode *root = new TreeNode();
+        root->left = build(head, left, mid - 1);
+        root->val = head->val;
+        head = head->next;
+        root->right = build(head, mid + 1, right);
+        return root;
+    }
+    TreeNode *sortedListToBST(ListNode *head) {
+        int length = 0;
+        ListNode *p = head;
+        while (p) {
+            p = p->next;
+            length++;
+        }
+        return build(head, 0, length - 1);
+    }
+};
+// @lc code=end
