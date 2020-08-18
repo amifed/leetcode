42771827567548c2c199fdb004574026e7947deb
dingjinyang <djy.dingjinyang@gmail.com>
2020-08-18 08:49:49 +0800

    109

--- linked-list-medium/109.有序链表转换二叉搜索树.cpp
+++ linked-list-medium/109.有序链表转换二叉搜索树.cpp
@@ -23,31 +23,49 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-private:
-  ListNode *findMiddleNode(ListNode *head) {
-    ListNode *pre = NULL, *slow = head, *fast = head;
-    while (fast != NULL && fast->next != NULL) {
-      pre = slow;
-      slow = slow->next;
-      fast = fast->next->next;
-    }
-    if (pre)
-      pre->next = NULL;
-    return slow;
-  }
-
 public:
-  TreeNode *sortedListToBST(ListNode *head) {
-    if (head == NULL)
-      return NULL;
-    ListNode *mid = findMiddleNode(head);
-    TreeNode *root = new TreeNode(mid->val);
-    if (mid == head)
-      return root;
-    root->left = sortedListToBST(head);
-    root->right = sortedListToBST(mid->next);
-    return root;
-  }
+    TreeNode *sortedListToBST(ListNode *head) {
+        if (!head) {
+            return nullptr;
+        }
+        ListNode *pre = nullptr, *slow = head, *fast = head;
+        while (fast && fast->next) {
+            pre = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        if (pre) {
+            pre->next = nullptr;
+        }
+        TreeNode *root = new TreeNode(slow->val);
+        if (slow == head) {
+            return root;
+        }
+        root->left = sortedListToBST(head);
+        root->right = sortedListToBST(slow->next);
+        return root;
+    }
 };
 // @lc code=end
