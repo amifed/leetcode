252b849ebee54dbc82de6720bd77b0e2eee63dec
dingjinyang <djy.dingjinyang@gmail.com>
2020-08-19 23:42:49 +0800

    update contestTemplate

--- contestTemplate.cpp
+++ contestTemplate.cpp
@@ -5,16 +5,19 @@ using LL = long long;
 using PII = pair<int, int>;
 using PIL = pair<int, LL>;
 using PLL = pair<LL, LL>;
-const int mod = 1e9 + 7;
-const int inf = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 7;
 
 class Solution {
 public:
-
 };
 
 int main() {
     Solution solution;
-
+    int a, b, c;
+    cin >> a >> b >> c;
+    string s;
+    cin >> s;
+    cout << (a + b + c) << ' ' << s << endl;
     return 0;
 }
