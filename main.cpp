955452acd0c7c02a0ad0f035c7580218cab4c2b0
dingjinyang <djy.dingjinyang@gmail.com>
2020-08-21 14:56:50 +0800

    add main

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+#define LOCAL
+int main(int argc, char const *argv[]) {
+#ifdef LOCAL
+    freopen("data.in", "r", stdin);
+    freopen("data.out", "w", stdout);
+#endif
+    int a, b;
+    while (~scanf("%d %d", &a, &b)) {
+        printf("%d %d\n", a, b);
+    }
+    return 0;
+}
--- main.py
+++
@@ -1,3 +0,0 @@
-print(int("1001111001", base=2))
-print(int("1011001101", base=2))
-print(int("1001111001", base=2))
