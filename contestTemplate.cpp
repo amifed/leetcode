f6d15787ba6885f2c8e86717738ded9add8c4ef3
dingjinyang <djy.dingjinyang@gmail.com>
2020-08-23 18:41:58 +0800

    consestTemplate

--- contestTemplate.cpp
+++ contestTemplate.cpp
@@ -15,33 +15,10 @@ const int INF = 1e9 + 7;
 
 class Solution {
 public:
+
 };
 
 int main() {
-    int m, n;
-    cin >> m >> n;
-    vector<string> v(m);
-    for (int i = 0; i < m; i++) {
-        cin >> v[i];
-    }
-    if (m == 0 || n == 0) {
-        cout << 0 << endl;
-        return 0;
-    }
-    int ret = 0;
-    VVI f(m, VI(n, 0));
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            if (v[i][j] == '1') {
-                if (i == 0 || j == 0) {
-                    f[i][j] = 1;
-                } else {
-                    f[i][j] = min({f[i - 1][j], f[i][j - 1], f[i - 1][j - 1]}) + 1;
-                }
-                ret = max(ret, f[i][j]);
-            }
-        }
-    }
-    cout << ret * ret << endl;
+    Solution sn;
     return 0;
 }
