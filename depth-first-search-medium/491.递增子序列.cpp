fab1afc97a77d3d76d272080c98e69fb42a80ab6
dingjinyang <djy.dingjinyang@gmail.com>
2020-08-25 08:53:27 +0800

    491

--- depth-first-search-medium/491.递增子序列.cpp
+++ depth-first-search-medium/491.递增子序列.cpp
@@ -6,29 +6,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 // @lc code=start
-class Solution {
+/* class Solution {
 public:
-    vector<int> track;
-    vector<vector<int>> ret;
-    void backtracking(vector<int> &nums, int start) {
-        if (track.size() > 1) {
-            ret.push_back(track);
-        }
-        unordered_set<int> ust;
-        for (int i = start; i < nums.size(); i++) {
-            if (track.size() > 0 && track.back() > nums[i] || ust.find(nums[i]) != ust.end()) {
-                continue;
+    int n;
+    vector<int> tmp;
+    vector<vector<int>> ans;
+    unordered_set<int> st;
+    void findSubsequences(int mask, vector<int> &nums) {
+        tmp.clear();
+        for (int i = 0; i < n; i++) {
+            if (mask & 1) {
+                tmp.push_back(nums[i]);
             }
-            ust.insert(nums[i]);
-            track.push_back(nums[i]);
-            backtracking(nums, i + 1);
-            track.pop_back();
+            mask >>= 1;
         }
+    }
+    bool check() {
+        for (int i = 1; i < tmp.size(); i++) {
+            if (tmp[i] < tmp[i - 1]) {
+                return false;
+            }
+        }
+        return tmp.size() > 1;
+    }
+    int getHash(int base, int mod) {
+        int hash = 0;
+        for (auto &x : tmp) {
+            hash = 1LL * hash * base % mod + (x + 101);
+            hash %= mod;
+        }
+        return hash;
     }
     vector<vector<int>> findSubsequences(vector<int> &nums) {
-        backtracking(nums, 0);
-        return ret;
+        n = nums.size();
+        for (int i = 0; i < (1 << n); i++) {
+            findSubsequences(i, nums);
+            int hash = getHash(263, 1e9 + 7);
+            if (check() && st.find(hash) == st.end()) {
+                st.insert(hash);
+                ans.push_back(tmp);
+            }
+        }
+        return ans;
     }
-};
+}; */
 // @lc code=end
 // [1,2,3,4,5,6,7,8,9,10,1,1,1,1,1]
